1c18099d48b961b4d68ac56e72d633643c4c45e2
ker <ker3rd@gmail.com>
2018-09-19 10:52:05 +0900

    ver1.1

--- Test2/Test2/main.c
+++ Test2/Test2/main.c
@@ -14,6 +14,7 @@ volatile uint8_t Hundred = 0, Ten = 0, One = 0;
 volatile uint8_t numbers[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x27, 0x7f, 0x67};
 //0		1		2	3		4	5		6	7		8	9
 //0011 1111, 0000 0110, 0101 1011, 0100 1111, 0110 0110, 0110 1101, 0111 1101, 0010 0111, 0111 1111, 0110 0111
+int 1.0_new_a;		//1.0 수정했음
 
 #define PRESCALER 1024	//분주비
 void Timer_init(void)
